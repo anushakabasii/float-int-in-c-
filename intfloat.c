087ebcf48ca3c33f53f24e7b93770730e7a251c3
Anusha Kabasi <115303715+anushakabasii@users.noreply.github.com>
2023-04-30 13:39:32 -0700

    Create intfloat.c

---
+++ intfloat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b;
+    float m,n;
+
+    scanf("%d%d",&a,&b);
+
+    scanf("%f%f",&m,&n);
+    printf("%d ",a+b);
+    printf("%d\n",a-b);
+    printf("%.1f ",m+n);
+    printf("%.1f\n",m-n);
+    return 0;
+    
+    
+    return 0;
+}
